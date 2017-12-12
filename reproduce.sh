ec1cf2390df585c57df7dead90b8324dbb6bdaf3
Zaur <zaur@genua.de>
2017-12-12 18:12:15 +0100

    One commit

---
+++ reproduce.sh
@@ -0,0 +1,5 @@
+printf "\n\nputs 'Environment:'; puts Rails.env;\n\nRails.configuration.sample" | rails console 2>/dev/null
+echo "---- this is right "
+printf "\n\nputs 'Environment:'; puts Rails.env;\n\nRails.configuration.sample" | rails console --environment test 2>/dev/null
+echo "---- this is wrong, outputs dev, but"
+echo 'it shall be {"database"=>"test"}'
