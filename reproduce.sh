dba8346d598e2701be1048d8da05189d698c446c
Zaur <zaur@genua.de>
2017-12-12 18:18:59 +0100

    More samples

--- reproduce.sh
+++ reproduce.sh
@@ -1,5 +1,9 @@
 printf "\n\nputs 'Environment:'; puts Rails.env;\n\nRails.configuration.sample" | rails console 2>/dev/null
 echo "---- this is right "
+
+printf "\n\nputs 'Environment:'; puts Rails.env;\n\nRails.configuration.sample" | rails console --environment=test 2>/dev/null
+echo "---- this is right too! "
+
 printf "\n\nputs 'Environment:'; puts Rails.env;\n\nRails.configuration.sample" | rails console --environment test 2>/dev/null
 echo "---- this is wrong, outputs dev, but"
 echo 'it shall be {"database"=>"test"}'
